695556583449bd99952e669bc733b1d3aba58d4d
Remi Mercier <remi.merci@gmail.com>
2018-07-06 16:35:22 +0200

    Handle JS loading of comments

--- public/index.js
+++ public/index.js
@@ -1,24 +1,8 @@
-// const bob = (event) => {
-//   console.log('Bob');
-// }
-
-// const bindBob = (element) => {
-//   document.querySelectorAll
-// }
-
-
-// document.querySelectorAll(".load-button").forEach((post) => {
-//   post.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     console.log('Bob');
-//   });
-// });
-
-
-document.querySelectorAll(".load-button").forEach((article) => {
-  article.addEventListener("click", (event) => {
+document.querySelectorAll('.load-button').forEach((button) => {
+  button.addEventListener('click', (event) => {
     event.preventDefault();
-    let rightContainer = document.querySelector(".right-container")
-    rightContainer.insertAdjacentHTML('afterend', '<%= @comments %>');
+    let comments = button.parentNode.nextElementSibling;
+    button.classList.toggle('green-placeholder');
+    comments.classList.toggle('hidden');
   });
 });
